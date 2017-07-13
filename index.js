dafe4917d4a662050e0c53ba14129c368e60cef0
Martin Tribo <martin.tribo@gmail.com>
2017-07-13 01:34:18 -0500

    Remove react references to reduce confusion

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-// import derp from './index.html';
-// import 'aframe';
-// import parser from 'html2hscript';
-// import 'aframe-orbit-controls-component-2';
-// import App from './reactscene';
-// import { AppContainer } from 'react-hot-loader';
-import socketio from 'socket.io/lib/client';
+// import socketio from 'socket.io/lib/client';
 // window.io = socketio;
 import createElement from 'virtual-dom/create-element';
 import diff from 'virtual-dom/diff';
 import patch from 'virtual-dom/patch';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
 let tree = require('./html-hscript-loader!./scene.html');
 const sceneNode = createElement(tree);
 
 window.onload = function () {
-  require('./aframesetup'); // A-Frame applies styles to the body, so need to load after that
+  require('./aframesetup'); // A-Frame applies styles to the body, so need to load after the page is loaded
   document.querySelector('#container').appendChild(sceneNode);
 };
 
 if (module.hot) {
-  module.hot.accept('./reactscene', () => { console.log('update!'); render(App); });
   module.hot.accept('./html-hscript-loader!./scene.html', function () {
     console.log('swap');
     var newTree = require('./html-hscript-loader!./scene.html');
@@ -31,6 +21,3 @@ if (module.hot) {
     tree = newTree;
   });
 }
-
-// import 'easyrtc';
-// import 'networked-aframe';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 var webpack = require('webpack');
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     './index'
   ],
